ebd31fbeec334fba638ac6951bee2936fd466930
Ilmari Hopkins <ilmarihopkins@gmail.com>
2024-09-11 21:43:54 +0200

    Update main.py without dotenv

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
